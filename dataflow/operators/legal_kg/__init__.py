8df717aefa5879fb39d09263d613cb52cdfb0e63
ZhP-Li197 <zhengpinli197@gmail.com>
2026-03-05 23:53:44 +0800

    Update tkg

---
+++ dataflow/operators/legal_kg/__init__.py
@@ -0,0 +1,16 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    # from .
+
+    
+else:
+    import sys
+    from dataflow.utils.registry import LazyLoader, generate_import_structure_from_type_checking
+
+
+    cur_path = "dataflow/operators/legal_kg/"
+
+
+    _import_structure = generate_import_structure_from_type_checking(__file__, cur_path)
+    sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/legal_kg/", _import_structure)
